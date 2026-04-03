6186def80b01296e23e04f6b6e1ce87cc9aadbbf
raindrops33 <268977837+raindrops30@users.noreply.github.com>
2026-04-03 11:45:40 +0100

    create buttons and display

--- main.js
+++ main.js
@@ -1,1 +1,29 @@
 console.log("hello World!");
+
+
+function add (a, b) {
+    let sum = a + b;
+    return sum;
+}
+
+function subtract (a, b) {
+    let sum = a - b;
+    return sum;
+}
+
+function multiply (a, b) {
+    let sum = a * b;
+    return sum;
+}
+
+function divide (a, b) {
+    let sum = a / b;
+    return sum;
+}
+
+
+
+console.log(add(5, 10));
+console.log(subtract(100, 10));
+console.log(multiply(4, 5));
+console.log(divide(30, 5));
